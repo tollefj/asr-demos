a80cc5260cdcbbd390cd40ed931f5cf692c03914
Tollef Jørgensen <tollefj@gmail.com>
2023-10-30 09:11:23 +0100

    updated app info

--- app/api/app.py
+++ app/api/app.py
@@ -1,9 +1,6 @@
 import flask
-from flask_cors import CORS, cross_origin
-
-import json
+from flask_cors import CORS
 import pandas as pd
-import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
 
@@ -16,8 +13,6 @@ SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8080
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config['CORS_ORIGINS'] = '*'
-
 
 index = faiss.read_index("knn.index", faiss.IO_FLAG_MMAP | faiss.IO_FLAG_READ_ONLY)
 print(f"Loading s-bert model")
@@ -29,8 +24,6 @@ def query(q, K=1):
     emb = model.encode([q], show_progress_bar=False)
     _, matches = index.search(emb, K)
     res = df.iloc[matches[0]]
-    # from the start and end columns
-    # return a list of objects with start and end
     return [
         {
             "start": row["start"],
@@ -46,23 +39,14 @@ def make_response(error: bool, message: str) -> flask.Response:
 def health_check() -> str:
     return f"app is running on {SERVER_HOST}:{SERVER_PORT}"
 
-default_timestamps = [1,2,3]
-
 @app.route('/search', methods=['POST'])
 def predictions() -> flask.Response:
     text = flask.request.json.get('text')
     k = flask.request.json.get('k')
     print(text)
-    # find the top K similar sentences from the computed index
-    # return a list of timestamps (start, end) for the matched sentences in the dataframe
     query_result = query(text, K=k)
     print("result", query_result)
     return flask.jsonify(query_result)
-
-# also set up a GET for /search
-@app.route('/search', methods=['GET'])
-def predictions_all() -> flask.Response:
-    return flask.jsonify(default_timestamps)
 
 app.run(host=SERVER_HOST, port=SERVER_PORT, debug=True, threaded=True)
 
